1476118298897afbfcc1e929514047ad48b471fd
Alokdev07 <alokbhuyan162@gmail.com>
2025-01-04 20:27:03 +0530

    Adding last hashQuestions

--- Hasq.java
+++ Hasq.java
@@ -36,15 +36,15 @@ public class Hasq {
         System.out.println(set.size());
     }
 
-    public static void intersectionOfTwo(int arr[],int arr1[]){
+    public static void intersectionOfTwo(int arr[], int arr1[]) {
         HashSet<Integer> interSet = new HashSet<>();
 
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<arr.length;i++){
-                set.add(arr[i]);
-            }
-        
-        for(int j=0;j<arr1.length;j++){
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+
+        for (int j = 0; j < arr1.length; j++) {
             if (set.contains(arr1[j])) {
                 interSet.add(arr1[j]);
             }
@@ -56,8 +56,8 @@ public class Hasq {
         }
     }
 
-    public static String getStart(HashMap<String,String> tick){
-        HashMap<String,String> revMap =  new HashMap<>();
+    public static String getStart(HashMap<String, String> tick) {
+        HashMap<String, String> revMap = new HashMap<>();
 
         for (String keys : tick.keySet()) {
             revMap.put(tick.get(keys), keys);
@@ -70,47 +70,65 @@ public class Hasq {
         return null;
     }
 
-    class solution{
-        public static int getLongestSubarray(int []a,int k){
-            HashMap<Integer,Integer> map = new HashMap<>();
-            int n =  a.length;
-            int sum = 0;
-            int maxLength = 0;
-            for(int i = 0;i<n;i++){
-                sum += a[i];
-                if (sum == k) {
-                    maxLength = Math.max(maxLength, i+1);
-                }
-                int remainingIndex = sum-k;
-                if (map.containsKey(rem)) {
-                    int len = i - map.get(rem);
-                    maxLength = Math.max(maxLength, len);
-                }
-                if (!map.containsKey(sum)) {
-                    map.put(sum, i);
-                }
+    public static int getLongestSubarray(int[] a, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = a.length;
+        int sum = 0;
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+            if (sum == k) {
+                maxLength = Math.max(maxLength, i + 1);
             }
-            return maxLength;
+            int remainingIndex = sum - k;
+            if (map.containsKey(rem)) {
+                int len = i - map.get(rem);
+                maxLength = Math.max(maxLength, len);
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
         }
-    }
-   
-    public static void main(String[] args) {
-        int num[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
-        // majorityElement(num);
-        int arr[] = { 7, 3, 9 };
-        int arr1[] = { 6, 3, 9, 2, 9, 4 };
-        
-        HashMap<String,String> tickets = new HashMap<>();
-        tickets.put("Chennai", "Bengaluru");
-        tickets.put("Mumbai", "Delhi");
-        tickets.put("Goa", "Chennai");
-        tickets.put("Delhi", "Goa");
-
-        String start = getStart(tickets);
-        while (tickets.containsKey(start)) {
-            System.out.print(start + "->");
-            start = tickets.get(start);
-        }
-        System.out.println(start);
+        return maxLength;
     }
 }
+
+public static int getSubArray(int[] arr,int k){
+    if (arr == 0 || arr.length == 0) {
+        return 0;
+    }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int n = arr.length;
+        int sum = 0;
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            sum += arr[i];
+            int remove = sum-k;
+            count += map.getOrDefault(remove, 0);
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+        return count;
+    }
+
+public static void main(String[] args) {
+    int num[] = { 1, 3, 2, 5, 1, 3, 1, 5, 1 };
+    // majorityElement(num);
+    int[] arr = {10,2,-2,-20,10 };
+    int arr1[] = { 6, 3, 9, 2, 9, 4 };
+
+    HashMap<String, String> tickets = new HashMap<>();
+    tickets.put("Chennai", "Bengaluru");
+    tickets.put("Mumbai", "Delhi");
+    tickets.put("Goa", "Chennai");
+    tickets.put("Delhi", "Goa");
+
+    String start = getStart(tickets);
+    while (tickets.containsKey(start)) {
+        System.out.print(start + "->");
+        start = tickets.get(start);
+    }
+    System.out.println(start);
+    System.out.println(getSubArray(arr1, -10));
+}
+
