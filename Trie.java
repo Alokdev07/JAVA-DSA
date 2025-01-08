34ae399ba186cc42de427b0aa2a88ad9118a80de
Alokdev07 <alokbhuyan162@gmail.com>
2025-01-08 18:11:31 +0530

    search method add in Trie and correct in insert method

--- Trie.java
+++ Trie.java
@@ -12,21 +12,41 @@ public class Trie {
     }
     static Node root = new Node();
     public static void insert(String word){
+        Node Current = root;
         for (int i = 0; i < word.length(); i++) {
             int idx = word.charAt(i) - 'a';
-            if (root.children[idx] == null) {
-                root.children[idx] = new Node();
+            if (Current.children[idx] == null) {
+                Current.children[idx] = new Node();
             }
+
             if (i == word.length() - 1) {
-                root.children[idx].endOfWord == true;
+                Current.children[idx].endOfWord = true;
             }
-            root = root.children[idx];
+            Current = Current.children[idx];
         }
+    }
+    public static boolean search(String word){
+        Node Current = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (Current.children[idx] == null) {
+                return false;
+            }
+            if (i == word.length()-1 && Current.children[idx].endOfWord == false) {
+                return false;
+            }
+            Current = Current.children[idx];
+        }
+        return true;
     }
     public static void main(String[] args) {
         String word[] = {"the","a","there" ,"their","any"};
         for (int i = 0; i < word.length; i++) {
             insert(word[i]);
         }
+       System.out.println(search("their"));
+       System.out.println(search("there"));
+       System.out.println(search("a"));
+       System.out.println(search("an"));
     }
 }
